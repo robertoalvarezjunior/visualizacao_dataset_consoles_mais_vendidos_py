8783b80e2f91976eeddf562287cf64e8e41658a3
roberto <roberto.junior@cfmv.gov.br>
2023-05-17 15:16:15 -0300

    refatorado

--- visu_dataset.py
+++ visu_dataset.py
@@ -11,32 +11,34 @@ data = pd.read_csv(r"C:\Users\roberto.junior\Desktop\best_selling_game_consoles.
 
 def grafico_barras(DataBar):
     colors = ["black", (0.33, 0.33, 0.33, 1.0), "gray", (0.66, 0.66, 0.66, 1.0)]
+    console_names = DataBar["Console Name"]
+    units_sold = DataBar["Units sold (million)"]
+    x_ticks = range(len(console_names))
 
-    plt.bar(
-        DataBar["Console Name"],
-        DataBar["Units sold (million)"],
-        color=colors,
-    )
-    plt.xlabel("Consoles")
-    plt.ylabel("Unidades Vendidas (Milhão)")
-    plt.title("Consoles mais vendidos de 1976 a 2020")
-    plt.xticks(rotation=90)
+    fig, ax = plt.subplots()
+    ax.bar(x_ticks, units_sold, color=colors)
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(console_names, rotation=90)
+    ax.set_xlabel("Consoles")
+    ax.set_ylabel("Unidades Vendidas (Milhão)")
+    ax.set_title("Consoles mais vendidos de 1976 a 2020")
     plt.show()
 
 
 def grafico_temp(DataTemp):
     DataTemp = DataTemp.sort_values(by="Released Year", ascending=True)
 
-    plt.plot(
+    fig, ax = plt.subplots()
+    ax.plot(
         DataTemp["Console Name"],
         DataTemp["Released Year"],
         marker="o",
         color="black",
     )
-    plt.xlabel("Consoles")
-    plt.ylabel("Ano lançamento")
-    plt.title("Ano de lançamento dos consoles mais vendidos")
-    plt.xticks(rotation=90)
+    ax.set_xlabel("Consoles")
+    ax.set_ylabel("Ano lançamento")
+    ax.set_title("Ano de lançamento dos consoles mais vendidos")
+    ax.tick_params(axis="x", rotation=90)
     plt.show()
 
 
